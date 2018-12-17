dfb8d2c2c5b705ac397903b0c719d0cf04f0865b
Peturs94 <petursson94@gmail.com>
2018-12-17 23:19:58 +0000

    cool

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, Button } from 'react-mdl';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
   render() {
@@ -12,6 +13,19 @@ class Landing extends Component {
             <div className="banner-text">
               <h1>Smiley Coin</h1>
               <p>The cryptocurrency used for rewarding work in the tutor-web.</p>
+            </div>
+            <div className="lysing-text">
+              <p>The Smileycoin,  SMLY, is used for rewarding
+               students for their performance in the tutor-web learning
+                environment and is used for donations to the project
+                Education in a Suitcase. This documents describes the foundations
+                 of the SMLY. At the bottom of the document there is a
+                  list of proposed changes.
+              </p>
+              <Button ripple>
+                <Link to="/aboutme">Learn More</Link>
+              </Button>
+
             </div>
           </Cell>
         </Grid>
