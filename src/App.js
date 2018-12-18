651af0fceb9520d215197130bd456ce5ae454268
Peturs94 <petursson94@gmail.com>
2018-12-18 23:54:52 +0000

    .

--- src/App.js
+++ src/App.js
@@ -10,21 +10,21 @@ import Backdrop from './components/Backdrop/Backdrop';
 import './Assets/css/default.min.css';
 
 class App extends Component {
-  state = {
+  state = { /*þessi breyta sér um að sýna drawerinn þegar síðan er orðin það lítil að headerinn sést ekki*/
     sideDrawerOpen: false
   };
 
-  drawerToggleClickHandler = () => {
+  drawerToggleClickHandler = () => { /*Dregur fram drawerinn þegar klikkað er á strikinn í vinstra horninu*/
     this.setState((prevState) => {
       return {sideDrawerOpen: !prevState.sideDrawerOpen};
     });
   };
 
-  backdropClickHandler = () => {
+  backdropClickHandler = () => { /*afmarka hægri hliðinná á síðunni til að fókusa á drawer elementinum*/
     this.setState({sideDrawerOpen: false});
   };
 
-  render() {
+  render() { 
     let backdrop;
 
     if (this.state.sideDrawerOpen) {
