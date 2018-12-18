1492590764d696e10c4c639097d33c1a8ec3002d
peturlogi <plp2@hi.is>
2018-12-18 23:53:53 +0000

    Smiley Uses tilbuin

--- src/components/Uses/Uses.js
+++ src/components/Uses/Uses.js
@@ -9,21 +9,26 @@ class Uses extends Component {
         <section className="uses__section">
           <h2>Smileycoin games and apps in the Google Playstore</h2>
 
-          <p>The Google Playstore has two Android apps which support the SmileyCoin.(SMLY)</p>
-          <p>The SmileyCoin(SMLY) wallet Android is currently the easiest SMLY wallet available.</p>
-          <p>The Smileycoin Bubble game is a simple computer game where you can use SMLY to buy certain features.</p>
-        </section>
+          <div className="uses__text">
+            <p>The Google Playstore has two Android apps which support the SmileyCoin.(SMLY)</p>
+            <p>The SmileyCoin(SMLY) wallet Android is currently the easiest SMLY wallet available.</p>
+            <p>The Smileycoin Bubble game is a simple computer game where you can use SMLY to buy certain features.</p>
+          </div>
+          </section>
 
         <section className="uses__section">
           <h2>Cryptocurrency exchanges</h2>
           <h3>Cryptocurrency exchanges and other ways to get into cryptocoins</h3>
 
-          <ul>
-            <li><a href="https://www.cryptopia.co.nz/">Cryptopia - (an exchange accepting SMLY and exchanging to/from Dogecoin and Litecoin; includes a market)</a></li>
-            <li><a href="https://novaexchange.com/market/LTC_SMLY/">Novaexchange - (an exchange accepting SMLY and exchanging to/from BTC and LTC; includes a simple game of dice)</a></li>
-            <li><a href="https://www.virwox.com/">VirWox - (the easiest way to buy SMLY for real money: First transfer $/ERU, then buy SLL; then buy BTC; then transfer these to C-CEX; then buy SMLY)</a></li>
-            <li><a href="http://educationinasuitcase.com/">It is also possible to donate to Education in a suitcase and receive some SMLY.</a></li>
-          </ul>
+          <div className="uses__text">
+            <ul>
+              <li><a href="https://www.cryptopia.co.nz/">Cryptopia - (an exchange accepting SMLY and exchanging to/from Dogecoin and Litecoin; includes a market)</a></li>
+              <li><a href="https://novaexchange.com/market/LTC_SMLY/">Novaexchange - (an exchange accepting SMLY and exchanging to/from BTC and LTC; includes a simple game of dice)</a></li>
+              <li><a href="https://www.virwox.com/">VirWox - (the easiest way to buy SMLY for real money: First transfer $/ERU, then buy SLL; then buy BTC; then transfer these to C-CEX; then buy SMLY)</a></li>
+              <li><a href="http://educationinasuitcase.com/">It is also possible to donate to Education in a suitcase and receive some SMLY.</a></li>
+            </ul>
+          </div>
+          
         </section>
       </div>
     )
